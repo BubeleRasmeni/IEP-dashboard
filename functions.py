c3c3da4e8b410482f08a5f0a2446d6e4f62b92f5
BubeleRasmeni <53602308+BubeleRasmeni@users.noreply.github.com>
2024-08-28 11:01:25 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -83,6 +83,6 @@ config_figure = {
         'filename': 'IEP_Figures',
         'height': 1500,
         'width': 3500,
-        'scale': 10  # Multiply title/legend/axis/canvas sizes by this factor
+        'scale': 8  # Multiply title/legend/axis/canvas sizes by this factor
     }
 }
