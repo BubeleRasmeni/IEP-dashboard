6fb435e809a681256b79562cfac6b13597652373
BubeleRasmeni <53602308+BubeleRasmeni@users.noreply.github.com>
2024-11-21 09:51:43 +0200

    Update about.py

--- about.py
+++ about.py
@@ -2,16 +2,23 @@ import streamlit as st
 import pandas as pd
 from functions import config_figure
 import plotly.express as px
+from css import app_css  # Import CSS as a string
+
+st.markdown(app_css, unsafe_allow_html=True)
+
+
 ###########
 @st.cache_data
 def load_data():
     # Load data from the Excel file
-    df = pd.read_excel('data/IEP_2017_2018_Stations.xlsx')
+    df = pd.read_excel("data/IEP_2017_2018_Stations.xlsx")
     return df
+
 
 stations = load_data()
 
-st.markdown("""
+st.markdown(
+    """
 <style>
 .small-text {
     font-size:16px !important;
@@ -21,7 +28,9 @@ st.markdown("""
     text-align: center;  /* Center-align the main title */
 }
 </style>
-""", unsafe_allow_html=True)
+""",
+    unsafe_allow_html=True,
+)
 
 # Create the layout with three columns
 col1, col2, col3 = st.columns([1, 18, 1])  # 5%, 90%, 5%
@@ -35,7 +44,8 @@ with col2:
     """
     st.markdown(title, unsafe_allow_html=True)
 
-    st.markdown("""
+    st.markdown(
+        """
     <p class="small-text" >
     Welcome to the Integrated Ecosystem Programme: Southern Benguela (IEP:SB) Analysis Dashboard.
     The IEP:SB is a multi-disciplinary project designed to undertake oceanographic research in the 
@@ -45,16 +55,21 @@ with col2:
     The data and insights gained from this program are crucial for ecosystem-based management and conservation efforts in the Southern Benguela region.
     It serves as a platform for collaboration and learning, bringing together students and researchers from various disciplines to study the complex interactions within this marine ecosystem.
     </p>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
 
-    st.markdown("""
+    st.markdown(
+        """
     <p class="small-text">
     This dashboard allows you to explore and visualise the CTD data collected during IEP voyages. 
     You can filter the data by grid, date range, and season, and visualize it through various types of plots, including ctd profiles, TS diagrams, and correlation heatmaps. 
     Each plot comes with a download option, enabling you to save the figures in different formats for further use offline.
     The data used here is published online in the South Africa's Marine Information Management System (MIMS). The link to the MIMS is catalogue available under the resource section.
     </p>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
 
 with col3:
     st.empty()  # Empty column to take 5% space on the right
@@ -64,27 +79,23 @@ col0, col1, col2 = st.columns([1, 18, 1])  # 5%, 90%, 5%
 
 with col1:
     # Scatter map of sampling stations
-    fig_stations = px.scatter_mapbox(stations, lat='Lat (°S)', lon='Lon (°E)', hover_name='Grid', zoom=5, height=600)
-    fig_stations.update_layout(mapbox_style="open-street-map", mapbox_center={"lat": -33.0, "lon": 20.0})
+    fig_stations = px.scatter_mapbox(
+        stations, lat="Lat (°S)", lon="Lon (°E)", hover_name="Grid", zoom=5, height=600
+    )
+    fig_stations.update_layout(
+        mapbox_style="open-street-map", mapbox_center={"lat": -33.0, "lon": 20.0}
+    )
     # Update marker style with different colors
     fig_stations.update_traces(
         marker=dict(
-            size=6,
-            symbol='circle',
-            opacity=0.7,
-            color='black'  # Example RGB color
+            size=6, symbol="circle", opacity=0.7, color="black"  # Example RGB color
         )
     )
-    
+
     # Update legend layout
     fig_stations.update_layout(
         title="Sampling Stations for IEP",
-        legend=dict(
-            title="Legend",
-            x=0.8,
-            y=1,
-            bgcolor='rgba(255, 255, 255, 0.7)'
-        )
+        legend=dict(title="Legend", x=0.8, y=1, bgcolor="rgba(255, 255, 255, 0.7)"),
     )
 
     st.plotly_chart(fig_stations, use_container_width=True, config=config_figure)
