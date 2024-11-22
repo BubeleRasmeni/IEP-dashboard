4c07e97de09fcf86dafddd2a1556de7dd14f66e1
BubeleRasmeni <53602308+BubeleRasmeni@users.noreply.github.com>
2024-11-22 16:29:35 +0200

    Update data_explorer.py

--- data_explorer.py
+++ data_explorer.py
@@ -24,7 +24,7 @@ st.markdown(
         font-weight: bold; 
         margin-bottom: 1rem; /* Add spacing below */
     '>
-        Data Exploration 📊 📉
+        Explore and Visualise Data 📊 📉
     </h1>
     """,
     unsafe_allow_html=True,
